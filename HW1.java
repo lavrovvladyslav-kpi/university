3204f75405e7fc065abd5e0752e5542f680ddaed
lavrovvladyslav-kpi <lavrov.vladyslav@lll.kpi.ua>
2025-09-09 14:50:22 +0300

    Join added

--- HW1.java
+++ HW1.java
@@ -5,12 +5,26 @@ public class HW1 {
 
 	        t1.start();
 	        t2.start();
+	        
+	        try {
+	        	t1.join();
+	        	t2.join();
+	        } catch (InterruptedException e){
+	        	e.printStackTrace();
+	        }
+	        
+	        System.out.println("All threads are finished");
 	    }
 
 	    private static void run(int start, int end, String name) {
 	        long product = 1;
 	        for (int i = start; i < end; i++) 
 	            product *= i;
+            try {
+                Thread.sleep(2000); // пауза 2000 мс
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 	        System.out.println(name + " is done. Result = " + product);
 	    }
 }
